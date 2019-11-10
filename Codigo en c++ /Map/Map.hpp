0aa8c87a3db9f039f2e642000f8ee75f82e8d9da
rafa <rafa.zamora.rals@gmail.com>
2019-11-10 13:12:34 -0600

    Creacion de dos funciones Retrieve y Remove que me devueven todo el Couple

--- Codigo en c++ /Map/Map.hpp
+++ Codigo en c++ /Map/Map.hpp
@@ -10,19 +10,21 @@
 template <typename ItemOne, typename ItemTwo>
 class Map{
 	private:
-		Couple<ItemOne,ItemTwo> *table;
 		size_t size;
 		size_t len;
 		ItemOne empty;
 		size_t (*hash)(ItemOne key, size_t max);
 		size_t (*coll_res)(size_t index, size_t max);
 	public:
+		Couple<ItemOne,ItemTwo> *table;		//La hice Publica para no hacer un getTable
 		Map(size_t capacity, ItemOne empty_val, size_t(*pHash)(ItemOne id, size_t max), size_t(*pCollRes)(size_t index, size_t max));
 		~Map();
 		bool Insert(ItemOne key, ItemTwo value);
 		/*Busca una clave y regresa su value*/
 		bool Retrieve(ItemOne key, ItemTwo* value);
+		bool Retrieve(ItemOne key, Couple<ItemOne,ItemTwo> *value);
 		bool Remove(ItemOne key);
+		bool Remove(ItemOne key, Couple<ItemOne,ItemTwo> *value);
 		size_t Iterator(void (*pfun)( ItemOne id, ItemTwo sueldo ));
 		size_t Len();
 		size_t Size();
@@ -105,6 +107,35 @@ bool Map<ItemOne,ItemTwo>::Retrieve(ItemOne key, ItemTwo* value){
 }
 
 template <typename ItemOne, typename ItemTwo>
+bool Map<ItemOne,ItemTwo>::Retrieve(ItemOne key, Couple<ItemOne,ItemTwo> *value){
+	int h=0;
+	size_t start_loc = this -> hash(key,this -> size);
+
+	size_t loc = start_loc;
+
+	bool more_to_search = true;
+	bool found;
+
+	do{
+		if(this ->table[loc].getOne() == key || this->table[loc].getOne() == this -> empty){
+			more_to_search = false;
+			found = this -> table[loc].getOne() == key;
+		}else{
+			h++;
+			loc = this -> coll_res(h*h,this -> size);
+		}
+	}while(loc != start_loc && more_to_search == true);
+
+
+
+	if(found == true){
+		*value = this->table[loc];
+	}
+
+	return found;
+}
+
+template <typename ItemOne, typename ItemTwo>
 bool Map<ItemOne,ItemTwo>::Remove(ItemOne key){
 	size_t loc = this -> hash(key,this -> size);
 	size_t i = loc;
@@ -113,6 +144,28 @@ bool Map<ItemOne,ItemTwo>::Remove(ItemOne key){
 
 	while(this -> table[loc].getOne() != this -> empty){
 		if(this ->table[loc].getOne() == key){
+			this -> table[loc].setOne(this -> empty);
+			--this -> len;
+			done = true;
+		}else{
+			h++;
+			loc = this -> coll_res(h*h,this -> size);
+		}
+	}
+
+	return done;
+}
+
+template <typename ItemOne, typename ItemTwo>
+bool Map<ItemOne,ItemTwo>::Remove(ItemOne key, Couple<ItemOne,ItemTwo> *value){
+	size_t loc = this -> hash(key,this -> size);
+	size_t i = loc;
+	int h= 0;
+	bool done = false;
+
+	while(this -> table[loc].getOne() != this -> empty){
+		if(this ->table[loc].getOne() == key){
+			*value = this -> table[loc];
 			this -> table[loc].setOne(this -> empty);
 			--this -> len;
 			done = true;
