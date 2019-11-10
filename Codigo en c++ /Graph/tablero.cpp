9fc0c71e110a74a03cacfab0138a22c9b4c0ce15
Teckwar159 <teckwar160@gmail.com>
2019-11-09 19:44:32 -0600

    Primer tablero

--- Codigo en c++ /Graph/tablero.cpp
+++ Codigo en c++ /Graph/tablero.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include "Graph.hpp"
 
+using namespace std;
+
+#define TAM 64
+
 int main(){
 
-	Graph tablero(64);
-	char letras[64] = {}
-	
+	Graph tablero(TAM);
+	string letras[TAM] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+					   "AA","BA","CA","DA","EA","FA","GA","HA","IA","JA","KA","LA","MA","NA","ÑA","OA","PA","QA","RA","SA","TA","UA","VA","WA","XA","YA","ZA"
+					   "AB","BB","CB","DB","EB","FB","GB","HB","IB","JB"};
+
+	for(size_t i = 0; i<TAM; i++){
+		tablero.addVertex(Vertex(letras[i]));
+	}
+
+	tablero.print();
+
 	return 0;
 }
